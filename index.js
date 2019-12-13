e6f7d2d6842968dbee53adae4bfa659fc21b72e3
Serhii <chuhaystyr@gmail.com>
2019-12-14 00:30:47 +0200

    task1

---
+++ index.js
@@ -0,0 +1,27 @@
+const { fromEvent } = rxjs
+const { debounceTime, map } = rxjs.operators
+
+const old = console.log
+const logger = document.getElementById('log')
+console.log = function(message) {
+  logger.innerHTML +=
+    '<span style="color:red">output: </span>' + message + '<br />';
+  old(message);
+}
+
+let res = {
+  input1: '',
+  input2: '',
+  input3: ''
+}
+
+function observableFactory(obj) {
+  for (const key in obj){
+    const input = document.getElementById(key)
+    fromEvent(input, 'input').pipe(
+      map(x => obj[key] = x.target.value)
+    ).subscribe(x => console.log(JSON.stringify(obj)))
+  }
+}
+
+observableFactory(res)
